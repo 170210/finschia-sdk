b3dce32d6c436bfe675f274da9f0c50d51843095
170210 <j170210@icloud.com>
2023-07-27 19:13:06 +0900

    test
    Signed-off-by: 170210 <j170210@icloud.com>

--- tools/error_doc/error_doc_generator.go
+++ tools/error_doc/error_doc_generator.go
@@ -4,23 +4,30 @@ import (
 	"fmt"
 
 	sdkerrors "github.com/Finschia/finschia-sdk/types/errors"
-	authz "github.com/Finschia/finschia-sdk/x/authz"
-	bank "github.com/Finschia/finschia-sdk/x/bank/types"
+	_ "github.com/Finschia/finschia-sdk/x/authz"
+	_ "github.com/Finschia/finschia-sdk/x/bank/types"
+	_ "github.com/Finschia/finschia-sdk/x/capability/types"
+	_ "github.com/Finschia/finschia-sdk/x/collection"
+	_ "github.com/Finschia/finschia-sdk/x/crisis/types"
+	_ "github.com/Finschia/finschia-sdk/x/distribution/types"
+	_ "github.com/Finschia/finschia-sdk/x/evidence/types"
+	_ "github.com/Finschia/finschia-sdk/x/feegrant"
+	_ "github.com/Finschia/finschia-sdk/x/gov/types"
+	_ "github.com/Finschia/finschia-sdk/x/params/types/proposal"
+	_ "github.com/Finschia/finschia-sdk/x/slashing/types"
+	_ "github.com/Finschia/finschia-sdk/x/staking/types"
+
+	_ "github.com/Finschia/finschia-sdk/x/token"
+	_ "github.com/Finschia/finschia-sdk/x/token/class"
 )
 
 func main() {
-	_ = authz.ErrInvalidExpirationTime
-
-	_ = bank.ErrNoInputs
-	_ = bank.ErrNoOutputs
-	_ = bank.ErrInputOutputMismatch
-	_ = bank.ErrSendDisabled
-	_ = bank.ErrDenomMetadataNotFound
-	_ = bank.ErrInvalidKey
 
 	errors := sdkerrors.RegisteredErrors()
 	for _, err := range errors {
-		fmt.Printf("Code: %d, CodeSpace: %s, Message: %s\n", err.ABCICode(), err.Codespace(), err.Error())
+		if err.Codespace() != "sdk" {
+			fmt.Printf("Code: %d, CodeSpace: %s, Message: %s\n", err.ABCICode(), err.Codespace(), err.Error())
+		}
 	}
 
 }
