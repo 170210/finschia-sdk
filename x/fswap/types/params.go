9fc8271994895c1de6fd57ee7f6a8c9f198a3a3a
170210 <j170210@icloud.com>
2024-04-16 18:04:44 +0900

    feat: init params & swapped
    Signed-off-by: 170210 <j170210@icloud.com>

--- x/fswap/types/params.go
+++ x/fswap/types/params.go
@@ -1,12 +1,23 @@
 package types
 
 import (
+	"errors"
+	fmt "fmt"
+	"strings"
+
 	"gopkg.in/yaml.v2"
 
+	sdk "github.com/Finschia/finschia-sdk/types"
 	paramtypes "github.com/Finschia/finschia-sdk/x/params/types"
 )
 
-var _ paramtypes.ParamSet = (*Params)(nil)
+const (
+	DefaultNewCoinDenom string = "PDT"
+)
+
+var (
+	ParamStoreKeyNewDenom = []byte("NewDenom")
+)
 
 // ParamKeyTable the param key table for launch module
 func ParamKeyTable() paramtypes.KeyTable {
@@ -14,22 +25,49 @@ func ParamKeyTable() paramtypes.KeyTable {
 }
 
 // NewParams creates a new Params instance
-func NewParams() Params {
-	return Params{}
+func NewParams(
+	newCoinDenom string,
+) Params {
+	return Params{
+		NewCoinDenom: newCoinDenom,
+	}
 }
 
 // DefaultParams returns a default set of parameters
 func DefaultParams() Params {
-	return NewParams()
+	return NewParams(
+		DefaultNewCoinDenom,
+	)
 }
 
 // ParamSetPairs get the params.ParamSet
 func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
-	return paramtypes.ParamSetPairs{}
+	return paramtypes.ParamSetPairs{
+		paramtypes.NewParamSetPair(ParamStoreKeyNewDenom, &p.NewCoinDenom, validateNewCoinDenom),
+	}
+}
+
+func validateNewCoinDenom(i interface{}) error {
+	v, ok := i.(string)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	if strings.TrimSpace(v) == "" {
+		return errors.New("new denom cannot be blank")
+	}
+	if err := sdk.ValidateDenom(v); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // Validate validates the set of params
 func (p Params) Validate() error {
+	if err := validateNewCoinDenom(p.NewCoinDenom); err != nil {
+		return err
+	}
 	return nil
 }
 
